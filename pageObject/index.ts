cef9f69263e67ee6a497d768d941c50f40af3769
e-nie <eniehaus111@gmail.com>
2024-10-02 01:43:10 +0200

    add HomePage class&spec

---
+++ pageObject/index.ts
@@ -0,0 +1,4 @@
+import {HomePage} from "./HomePage";
+
+
+export const homePage = new HomePage()
---
+++ utilities/data.ts
@@ -0,0 +1,10 @@
+export const navBarValue: string[] = [
+    'Home',
+    'Apparel & accessories',
+    'Makeup',
+    'Skincare',
+    'Fragrance',
+    'Men',
+    'Hair Care',
+    'Books',
+]
